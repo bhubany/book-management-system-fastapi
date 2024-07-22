c627559f7560c89e5b2704d407da10cd9b03bc64
bhubany <yadav.bhuban.by@gmail.com>
2024-07-22 22:27:15 +0545

    feat(): script to start server

---
+++ start_server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ -z "$VIRTUAL_ENV" ]]; then
+    echo "No virtual environment detected. Attempting to activate virtual environment..."
+    # Activate virtual environment if it exists
+    if [ -d "venv" ]; then
+        source venv/bin/activate
+        echo "Virtual environment activated."
+    else
+        echo "No virtual environment found. Please set up a virtual environment."
+        exit 1
+    fi
+else
+    echo "Virtual environment already active."
+fi
+
+# Start the FastAPI application
+echo "Starting FastAPI application..."
+fastapi dev app/main.py
