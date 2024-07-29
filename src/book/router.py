8f0564793ce3b30a180b85411091ffeb5fc59f37
bhubany <yadav.bhuban.by@gmail.com>
2024-07-29 23:41:13 +0545

    feat(): add custom api response

--- src/book/router.py
+++ src/book/router.py
@@ -3,24 +3,25 @@ from book.service import BookService
 from book.models import BookRequest, BookResponse
 from uuid import UUID
 from typing import List
+from common.schemas.response import ApiResponse
 
 
 class BookRouter:
     def __init__(self):
         self.service = BookService()
         self.router = APIRouter()
-        self.router.post("/", response_model=BookResponse)(self.save)
+        self.router.post("/", response_model=ApiResponse)(self.save)
         self.router.get(
-            "/{book_id}", response_model=BookResponse)(self.find_by_id)
-        self.router.get("", response_model=List[BookResponse])(self.find_all)
+            "/{book_id}", response_model=ApiResponse)(self.find_by_id)
+        self.router.get("", response_model=ApiResponse)(self.find_all)
 
-    def save(self, book_data: BookRequest) -> BookResponse:
+    def save(self, book_data: BookRequest) -> ApiResponse:
         return self.service.save(book_data)
 
-    def find_by_id(self, book_id: UUID) -> BookResponse:
+    def find_by_id(self, book_id: UUID) -> ApiResponse:
         return self.service.find_by_id(book_id)
 
-    def find_all(self) -> List[BookResponse]:
+    def find_all(self) -> ApiResponse:
         return self.service.find_all()
 
 
--- src/common/schemas/response.py
+++ src/common/schemas/response.py
@@ -1,9 +1,28 @@
-from fastapi.responses import JSONResponse, UJSONResponse
+from fastapi.responses import JSONResponse
+from typing import Dict, TypeVar, Generic, Optional
+from pydantic import BaseModel
+
+T = TypeVar('T')
 
 
-class ApiResponse():
-    def __init__(self, status, message, data):
-        self.status = status
-        self.message = message
-        self.data = data
-        self.error = None
+class Error(BaseModel):
+    """Base model for error responses."""
+    type: str
+    sub_code: int
+    title: str
+    user_message: str
+    support_message: str
+
+
+class __AppResponse(BaseModel, Generic[T]):
+    """Base model for app responses."""
+    data: Optional[T]
+    error: Error | None = None
+    status_code: int = 200
+
+
+class ApiResponse(JSONResponse):
+    def __init__(self, status: int, data: Generic[T] = None, error: Error = None, headers: Dict[str, str] = None):
+        response = __AppResponse(
+            status_code=status, data=data, error=error).model_dump()
+        super.__init__(content=response, status_code=status, headers=headers)
